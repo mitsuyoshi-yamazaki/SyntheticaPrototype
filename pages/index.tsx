a554ff684244e733b18902a9d2f6519e6addc74a
Yamazaki Mitsuyoshi <mitsuyoshi.tsukuba@gmail.com>
2025-06-06 09:32:21 +0900

    lint violationを修正

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,12 @@
-import Head from 'next/head'
-import dynamic from 'next/dynamic'
-import Layout from '../components/Layout'
+import Head from "next/head"
+import dynamic from "next/dynamic"
+import Layout from "../components/Layout"
 
 // p5.jsコンポーネントをSSRを無効にして動的インポート
-const Canvas = dynamic(() => import('../components/Canvas'), {
+const Canvas = dynamic(() => import("../components/Canvas"), {
   ssr: false,
   loading: () => (
-    <div className="w-full h-full flex items-center justify-center" style={{ minHeight: '400px' }}>
+    <div className="w-full h-full flex items-center justify-center" style={{ minHeight: "400px" }}>
       <p className="text-gray-600">Canvas Loading...</p>
     </div>
   )
